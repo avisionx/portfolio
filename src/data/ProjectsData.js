0d3087a7c271e9a5f1b6607bebea5ef129f11223
Avi Garg <avi.grg4567@gmail.com>
2021-12-21 21:52:17 +0530

    Update ProjectsData.js

--- src/data/ProjectsData.js
+++ src/data/ProjectsData.js
@@ -1,13 +1,5 @@
 const LiveWebsitesContent = [
   {
-    img: 'crams.jpg',
-    title: "CRAMS",
-    tags: ['django', 'python', 'frontend', 'backend', 'ui/ux'],
-    desc:
-      'CoFounder at CRAMS, a course recommendation and management system for IIITD.',
-    website: 'http://crams.ml/'
-  },
-  {
     img: 'odyssey.jpg',
     title: "ODYSSEY'20 IIITD",
     tags: ['flask', 'python', 'frontend', 'backend', 'ui/ux'],
